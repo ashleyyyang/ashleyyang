736548c4f822956a8058c8b2e8cf4f839da59cf6
Ashley Yang <43325293+ashleyyyang@users.noreply.github.com>
2019-04-03 22:36:10 -0400

    Add files via upload

--- js/font.js
+++ js/font.js
@@ -39,7 +39,7 @@ preload(
     "http://ashleyyang.ca/art/italy.jpg",
     "http://ashleyyang.ca/art/self-portrait.jpg"
 )
-*/
+
 
 
 function preloader() {
@@ -66,4 +66,23 @@ function addLoadEvent(func) {
 		}
 	}
 }
-addLoadEvent(preloader);
+addLoadEvent(preloader); */
+
+
+$(function(){
+  $.each(document.images, function(){
+             var this_image = this;
+             var src = $(this_image).attr('src') || '' ;
+             if(!src.length > 0){
+                 //this_image.src = options.loading; // show loading
+                 var lsrc = $(this_image).attr('lsrc') || '' ;
+                 if(lsrc.length > 0){
+                     var img = new Image();
+                     img.src = lsrc;
+                     $(img).load(function() {
+                         this_image.src = this.src;
+                     });
+                 }
+             }
+         });
+});
